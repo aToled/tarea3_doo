575fd39cee9653349d977fc9038e149841e5299a
kh12v <154763602+kh12v@users.noreply.github.com>
2025-05-29 21:05:17 -0400

    nuevo dulce añádido 'Chocman'

---
+++ src/main/java/logica/Chocman.java
@@ -0,0 +1,19 @@
+package logica;
+/**
+ * Instancia de Dulce que representa un Chocman, puede ser comprado, almacenado y consumido.
+ */
+public class Chocman extends Dulce {
+
+    /**
+     * @see Dulce
+     */
+    public Chocman() {
+        super();
+    }
+
+    /**
+     * El comprador almacena este valor cuando consume el producto
+     * @return String único que retorna este producto al consumirlo
+     */
+    public String consumir() { return "chocman"; }
+}
